b899052e8d0e87567a3123d6301d2314d52aa1ec
spout <snoopyxdy@gmail.com>
2014-04-22 17:04:11 +0800

    env

--- runserver.py
+++ runserver.py
@@ -1,16 +1,25 @@
+# -*- coding: utf-8 -*-
 from trymenu import app
 from trymenu import config
 from trymenu.controllers import *
 from trymenu.models import *
 import os
+import sys
+import getopt
 
-__env = os.environ.get("FLASKENV")
+__opts, _ = getopt.getopt(sys.argv[1:], "e:") #获取命令行参数
+__scritpEnv = ""
 
-if __env == "Production" :
+for name, value in __opts:
+    if name == "-e": #获取命令行参数e
+        __scritpEnv = value
+        
+
+if __scritpEnv == "Production" :
     app.config.from_object(config.Production())
 else:
     app.config.from_object(config.Debug())
-    
+
 if __name__ == '__main__':
     app.run(host=app.config.get("HOST"),port=app.config.get("PORT"))
     
